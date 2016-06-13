3f824dbc76d0c17f3e64cee805003538e874f86b
emjwivell <emjwivell@gmail.com>
2016-06-13 10:17:32 -0400

    clean up commit

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.views.generic import CreateView
-from django.http import HttpResponseRedirect
+from django.views.generic import CreateView, TemplateView, View
+
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 class SignUpView(CreateView):
@@ -13,6 +15,18 @@ class SignUpView(CreateView):
     def get_success_url(self):
         return reverse("login_view")
 
+
+class WhatView(TemplateView):
+    template_name = "what.html"
+
+
+class AwesomeAPIView(View):
+
+    def get(self, request):
+        # Awesome.objects.all()
+        print(request.user)
+        data = json.dumps({"joel": "awesome"})
+        return HttpResponse(data, content_type="application/json")
 
 def homepage(request):
     return HttpResponseRedirect(reverse('login_view'))
--- login/urls.py
+++ login/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from app.views import homepage, SignUpView
+from app.views import homepage, SignUpView, WhatView, AwesomeAPIView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', homepage),
+    url(r'^what/$', WhatView.as_view(), name="what_view"),
+    url(r'^api/awesome/$', AwesomeAPIView.as_view(), name="awesome_api_view"),
     url(r'^signup/$', SignUpView.as_view(), name="signup_view"),
     url(r'^login/$', auth_views.login, name="login_view"),
     url(r'^logout/$', auth_views.logout_then_login, name="logout_view"),
